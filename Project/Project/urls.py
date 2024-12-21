275b05f43665a65e05bb2e0d7ecf7fd1a608f1fb
Kaitru <kiryakrutyak@yandex.ru>
2024-12-21 22:58:50 +0300

    3 task

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('cart/', cart),
+    path('shop/', shop),
+    path('register/', register),
 ]
--- Project/task1/views.py
+++ Project/task1/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from task1.models import *
 
 # Create your views here.
+def index(request):
+    title = 'Главная страница'
+    context = {
+        'title': title,
+    }
+    return render(request, 'index.html', context)
+
+def shop(request):
+    title = 'Магазин'
+    products = Game.objects.all()
+    context = {
+        'title': title,
+        'products': products,
+    }
+    return render(request, 'shop.html', context)
+
+def cart(request):
+    title = 'Корзина'
+    context = {
+        'title': title,
+    }
+    return render(request, 'cart.html', context)
+
+def register(request):
+    users = Buyer.objects.all()
+    info = {}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        if password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        elif username in users:
+            info['error'] = 'Пользователь уже существует'
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        else:
+            for user in users:
+                if user.name == username:
+                    info['error'] = 'Пользователь уже существует'
+                    break
+            else:
+                Buyer.objects.create(name=username, balance=0, age=age)
+                info['success'] = f'Приветствуем, {username}'
+
+    return render(request, 'registration_page.html', info)
